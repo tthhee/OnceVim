a72b020a3f06de83bde46dd2df8f1e43c5fdd40a
OnceMore2020 <guanhwang2011@gmail.com>
2014-11-27 21:20:37 +0800

    initial

--- .vimrc
+++ .vimrc
@@ -765,52 +765,20 @@
     " }
 
     " vim-airline {
-        " Set configuration options for the statusline plugin vim-airline.
-        " Use the powerline theme and optionally enable powerline symbols.
-        " To use the symbols , , , , , , and .in the statusline
-        " segments add the following to your .vimrc.before.local file:
-        "   let g:airline_powerline_fonts=1
-        " If the previous symbols do not render for you then install a
-        " powerline enabled font.
-
-        " See `:echo g:airline_theme_map` for some more choices
         " Default in terminal vim is 'dark'
         if isdirectory(expand("~/.vim/bundle/vim-airline/"))
-            if !exists('g:airline_theme')
-                let g:airline_theme = 'solarized'
-            endif
-            if !exists('g:airline_powerline_fonts')
-                " Use the default set of separators with a few customizations
-                let g:airline_left_sep='›'  " Slightly fancier than '>'
-                let g:airline_right_sep='‹' " Slightly fancier than '<'
-            endif
+            let g:airline_theme = 'solarized'
+            let g:airline_powerline_fonts=1
         endif
     " }
 
 " }
 
 " GUI Settings {
-
-    " GVIM- (here instead of .gvimrc)
     if has('gui_running')
         set guioptions-=T           " Remove the toolbar
         set lines=40                " 40 lines of text instead of 24
-        if !exists("g:spf13_no_big_font")
-            if LINUX() && has("gui_running")
-                set guifont=Andale\ Mono\ Regular\ 12,Menlo\ Regular\ 11,Consolas\ Regular\ 12,Courier\ New\ Regular\ 14
-            elseif OSX() && has("gui_running")
-                set guifont=Andale\ Mono\ Regular:h12,Menlo\ Regular:h11,Consolas\ Regular:h12,Courier\ New\ Regular:h14
-            elseif WINDOWS() && has("gui_running")
-                set guifont=Andale_Mono:h10,Menlo:h10,Consolas:h10,Courier_New:h10
-            endif
-        endif
-    else
-        if &term == 'xterm' || &term == 'screen'
-            set t_Co=256            " Enable 256 colors to stop the CSApprox warning and make xterm vim shine
-        endif
-        "set term=builtin_ansi       " Make arrow and other keys work
     endif
-
 " }
 
 " Functions {
@@ -909,24 +877,4 @@
     " e.g. Grep current file for <search_term>: Shell grep -Hn <search_term> %
     " }
 
-" }
-
-" Use fork vimrc if available {
-    if filereadable(expand("~/.vimrc.fork"))
-        source ~/.vimrc.fork
-    endif
-" }
-
-" Use local vimrc if available {
-    if filereadable(expand("~/.vimrc.local"))
-        source ~/.vimrc.local
-    endif
-" }
-
-" Use local gvimrc if available and gui is running {
-    if has('gui_running')
-        if filereadable(expand("~/.gvimrc.local"))
-            source ~/.gvimrc.local
-        endif
-    endif
 " }
--- OnceVim.sh
+++ OnceVim.sh
@@ -5,7 +5,6 @@ app_dir="$HOME/.OnceVim"
 [ -z "$git_uri" ] && git_uri="https://github.com/OnceMore2020/OnceVim.git"
 [ -z "$VUNDLE_URI" ] && VUNDLE_URI="https://github.com/gmarik/vundle.git"
 
-############################  BASIC SETUP TOOLS
 msg() {
     printf '%b\n' "$1" >&2
 }
@@ -21,66 +20,17 @@ error() {
     exit 1
 }
 
-debug() {
-    if [ "$debug_mode" -eq '1' ] && [ "$ret" -gt '1' ]; then
-      msg "An error occurred in function \"${FUNCNAME[$i+1]}\" on line ${BASH_LINENO[$i+1]}, we're sorry for that."
-    fi
-}
-
-variable_set() {
-    if [ -z "$1" ]; then
-        error "You must have your HOME environmental variable set to continue."
-    fi
-}
-
-############################ SETUP FUNCTIONS
 lnif() {
     if [ -e "$1" ]; then
         ln -sf "$1" "$2"
     fi
     ret="$?"
-    debug
-}
-
-do_backup() {
-    if [ -e "$2" ] || [ -e "$3" ] || [ -e "$4" ]; then
-        today=`date +%Y%m%d_%s`
-        for i in "$2" "$3" "$4"; do
-            [ -e "$i" ] && [ ! -L "$i" ] && mv "$i" "$i.$today";
-        done
-        ret="$?"
-        success "$1"
-        debug
-   fi
-}
-
-upgrade_repo() {
-      msg "trying to update $1"
-
-      if [ "$1" = "$app_name" ]; then
-          cd "$app_dir" &&
-          git pull origin master 
-      fi
-
-      if [ "$1" = "vundle" ]; then
-          cd "$HOME/.vim/bundle/vundle" &&
-          git pull origin master
-      fi
-
-      ret="$?"
-      success "$2"
-      debug
 }
 
 clone_repo() {
-    if [ ! -e "$app_dir" ]; then
-        git clone "$git_uri" "$app_dir"
-        ret="$?"
-        success "$1"
-        debug
-    else
-        upgrade_repo "$app_name"    "Successfully updated $app_name"
-    fi
+   git clone "$git_uri" "$app_dir"
+   ret="$?"
+   success "$1"
 }
 
 clone_vundle() {
@@ -91,53 +41,35 @@ clone_vundle() {
     fi
     ret="$?"
     success "$1"
-    debug
 }
 
 create_symlinks() {
     endpath="$app_dir"
-
     if [ ! -d "$endpath/.vim/bundle" ]; then
         mkdir -p "$endpath/.vim/bundle"
     fi
-
     lnif "$endpath/.vimrc"              "$HOME/.vimrc"
     lnif "$endpath/.vimrc.bundles"      "$HOME/.vimrc.bundles"
     lnif "$endpath/.vim"                "$HOME/.vim"
-
     ret="$?"
     success "$1"
-    debug
 }
 
 setup_vundle() {
     system_shell="$SHELL"
     export SHELL='/bin/sh'
-    
     vim \
         -u "$app_dir/.vimrc.bundles" \
         "+set nomore" \
         "+BundleInstall!" \
         "+BundleClean" \
         "+qall"
-    
     export SHELL="$system_shell"
-
     success "$1"
-    debug
 }
 
-############################ MAIN()
-variable_set "$HOME"
-        "$HOME/.vim" \
-        "$HOME/.vimrc" \
-
-clone_repo      "Successfully cloned $app_name"
-
-create_symlinks "Setting up vim symlinks"
-
-clone_vundle    "Successfully cloned vundle"
-
-setup_vundle    "Now updating/installing plugins using Vundle"
-
-msg             "\nThanks for installing $app_name."
+clone_repo      "克隆 $app_name 到本地."
+create_symlinks "创建软链接"
+clone_vundle    "配置Vundle"
+setup_vundle    "开始安装"
+msg             "\n感谢使用$app_name."
